ac16b933b592783c9ce31c50ebb9c54859b1c449
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-20 16:14:24 +0200

    :sparkles: [#6281] Allow configuring additional document type options
    * confidentiality level
    * title
    
    Texts taken and adapted from the formio-builder to keep similarity with
    what's currently in the registration tab of the file component.

--- src/openforms/js/components/admin/form_design/registrations/objectsapi/edit_options/FileEditor.js
+++ src/openforms/js/components/admin/form_design/registrations/objectsapi/edit_options/FileEditor.js
@@ -4,6 +4,7 @@ import {FormattedMessage} from 'react-intl';
 import Field from 'components/admin/forms/Field';
 import FormRow from 'components/admin/forms/FormRow';
 import {TextInput} from 'components/admin/forms/Inputs';
+import ReactSelect from 'components/admin/forms/ReactSelect';
 import {TargetPathSelect} from 'components/admin/forms/objects_api';
 import ErrorMessage from 'components/errors/ErrorMessage';
 
@@ -85,9 +86,72 @@ const FileEditor = ({
         </Field>
       </FormRow>
 
+      <ConfidentialityLevel namePrefix={namePrefix} />
+
+      <FormRow>
+        <Field
+          name={`${namePrefix}.options.title`}
+          label={
+            <FormattedMessage
+              description="Document upload: title option label"
+              defaultMessage="Title"
+            />
+          }
+          helpText={
+            <FormattedMessage
+              description="Document upload: title option help text"
+              defaultMessage={`Optional custom title for the document. By default, the
+              form name is used.`}
+            />
+          }
+        >
+          <TextInput {...getFieldProps(`${namePrefix}.options.title`)} maxLength="200" />
+        </Field>
+      </FormRow>
+
       <ShowJSONSchemaToggle availablePaths={targetPaths} targetPath={mappedVariable.targetPath} />
     </>
   );
 };
+
+const CONFIDENTIALITY_OPTIONS = [
+  {value: 'openbaar', label: 'Openbaar'},
+  {value: 'beperkt_openbaar', label: 'Beperkt openbaar'},
+  {value: 'intern', label: 'Intern'},
+  {value: 'zaakvertrouwelijk', label: 'Zaakvertrouwelijk'},
+  {value: 'vertrouwelijk', label: 'Vertrouwelijk'},
+  {value: 'confidentieel', label: 'Confidentieel'},
+  {value: 'geheim', label: 'Geheim'},
+  {value: 'zeer_geheim', label: 'Zeer geheim'},
+];
+
+const ConfidentialityLevel = ({namePrefix}) => (
+  <FormRow>
+    <Field
+      name={`${namePrefix}.options.confidentialityLevel`}
+      label={
+        <FormattedMessage
+          description="Document upload: confidentialityLevel label"
+          defaultMessage="Confidentiality"
+        />
+      }
+      helpText={
+        <FormattedMessage
+          description="Document upload: confidentialityLevel option help text"
+          defaultMessage={`Indication of the level to which extent the document is meant
+          to be public. Only provide this if you wish to override the default from the
+          configured document type.`}
+        />
+      }
+      noManageChildProps
+    >
+      <ReactSelect
+        name={`${namePrefix}.options.confidentialityLevel`}
+        options={CONFIDENTIALITY_OPTIONS}
+        isClearable
+      />
+    </Field>
+  </FormRow>
+);
 
 export default FileEditor;
