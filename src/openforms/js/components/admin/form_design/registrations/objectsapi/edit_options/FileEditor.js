b16478ec6161f53801a6dd4b61bb5a103ad56b15
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-21 09:31:49 +0200

    :art: [#6281] Structure the code better to declutter

--- src/openforms/js/components/admin/form_design/registrations/objectsapi/edit_options/FileEditor.js
+++ src/openforms/js/components/admin/form_design/registrations/objectsapi/edit_options/FileEditor.js
@@ -1,4 +1,4 @@
-import {useFormikContext} from 'formik';
+import {useField, useFormikContext} from 'formik';
 import {FormattedMessage} from 'react-intl';
 
 import Field from 'components/admin/forms/Field';
@@ -24,7 +24,7 @@ const FileEditor = ({
   objecttypeVersion,
   backendOptions,
 }) => {
-  const {getFieldProps} = useFormikContext();
+  const {catalogue} = backendOptions;
 
   const variableSchema = useVariableJsonSchema(variable, components);
   const {loading, targetPaths, error} = useFetchTargetPaths({
@@ -34,7 +34,11 @@ const FileEditor = ({
     variableJsonSchema: variableSchema,
   });
 
-  if (error)
+  if (catalogue) {
+    console.log(catalogue);
+  }
+
+  if (error) {
     return (
       <ErrorMessage>
         <FormattedMessage
@@ -43,74 +47,64 @@ const FileEditor = ({
         />
       </ErrorMessage>
     );
+  }
 
   return (
     <>
-      <FormRow>
-        <Field
-          name={`${namePrefix}.targetPath`}
-          label={
-            <FormattedMessage
-              defaultMessage="JSON Schema target"
-              description="'JSON Schema target' label"
-            />
-          }
-          required
-          noManageChildProps
-        >
-          <TargetPathSelect
-            name={`${namePrefix}.targetPath`}
-            isLoading={loading}
-            targetPaths={targetPaths}
-          />
-        </Field>
-      </FormRow>
-      <FormRow>
-        <Field
-          name={`${namePrefix}.options.organizationRsin`}
-          label={
-            <FormattedMessage
-              description="Document upload: organizationRsin option label"
-              defaultMessage="Organization RSIN"
-            />
-          }
-          helpText={
-            <FormattedMessage
-              description="Document upload: organizationRsin option help text"
-              defaultMessage={`RSIN of the organization that registers the document in
-              the Documents API. If left blank, the general configuration is used.`}
-            />
-          }
-        >
-          <TextInput {...getFieldProps(`${namePrefix}.organizationRsin`)} maxLength="9" />
-        </Field>
-      </FormRow>
-
+      <TargetPath namePrefix={namePrefix} loading={loading} targetPaths={targetPaths} />
+      <OrganizationRSIN namePrefix={namePrefix} />
       <ConfidentialityLevel namePrefix={namePrefix} />
-
-      <FormRow>
-        <Field
-          name={`${namePrefix}.options.title`}
-          label={
-            <FormattedMessage
-              description="Document upload: title option label"
-              defaultMessage="Title"
-            />
-          }
-          helpText={
-            <FormattedMessage
-              description="Document upload: title option help text"
-              defaultMessage={`Optional custom title for the document. By default, the
-              form name is used.`}
-            />
-          }
-        >
-          <TextInput {...getFieldProps(`${namePrefix}.options.title`)} maxLength="200" />
-        </Field>
-      </FormRow>
-
+      <Title namePrefix={namePrefix} />
       <ShowJSONSchemaToggle availablePaths={targetPaths} targetPath={mappedVariable.targetPath} />
     </>
+  );
+};
+
+const TargetPath = ({namePrefix, loading, targetPaths}) => (
+  <FormRow>
+    <Field
+      name={`${namePrefix}.targetPath`}
+      label={
+        <FormattedMessage
+          defaultMessage="JSON Schema target"
+          description="'JSON Schema target' label"
+        />
+      }
+      required
+      noManageChildProps
+    >
+      <TargetPathSelect
+        name={`${namePrefix}.targetPath`}
+        isLoading={loading}
+        targetPaths={targetPaths}
+      />
+    </Field>
+  </FormRow>
+);
+
+const OrganizationRSIN = ({namePrefix}) => {
+  const [props] = useField(`${namePrefix}.options.organizationRsin`);
+  return (
+    <FormRow>
+      <Field
+        name={props.name}
+        label={
+          <FormattedMessage
+            description="Document upload: organizationRsin option label"
+            defaultMessage="Organization RSIN"
+          />
+        }
+        helpText={
+          <FormattedMessage
+            description="Document upload: organizationRsin option help text"
+            defaultMessage={`RSIN of the organization that registers the document in
+          the Documents API. If left blank, the general configuration is used.`}
+          />
+        }
+      >
+        <TextInput {...props} maxLength="9" />
+      </Field>
+    </FormRow>
   );
 };
 
@@ -153,5 +147,31 @@ const ConfidentialityLevel = ({namePrefix}) => (
     </Field>
   </FormRow>
 );
+
+const Title = ({namePrefix}) => {
+  const [props] = useField(`${namePrefix}.options.title`);
+  return (
+    <FormRow>
+      <Field
+        name={props.name}
+        label={
+          <FormattedMessage
+            description="Document upload: title option label"
+            defaultMessage="Title"
+          />
+        }
+        helpText={
+          <FormattedMessage
+            description="Document upload: title option help text"
+            defaultMessage={`Optional custom title for the document. By default, the
+            form name is used.`}
+          />
+        }
+      >
+        <TextInput {...props} maxLength="200" />
+      </Field>
+    </FormRow>
+  );
+};
 
 export default FileEditor;
